671b1edecc960b09344f8feb354c1e4d342ee3f9
Nahan <nahan7799@gmail..com>
2024-06-17 00:56:48 -0400

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{  
+    [SerializeField]
+  private float _speed = 3.5f;
+
+  private float _speedMultiplier = 2;
+  
+  [SerializeField]
+  private GameObject _laserPrefab;
+
+  [SerializeField]
+  private GameObject _tripleShotPrefab;
+
+  [SerializeField]
+  private bool _isTripleShotActive = false;
+  //private bool _isSpeedBoostActive = false;
+    // Start is called before the first frame update
+    private bool _isShieldsActive = false;
+
+    [SerializeField]
+    private GameObject _shieldVisualizer;
+
+   [SerializeField]
+    private float _fireRate = 0.15f;
+    private float _canfire = -1f;
+
+    [SerializeField]
+    private int _lives = 3;
+    private SpawnManager _spawnManager;
+
+    void Start()
+    {
+        // for when the player starts i want to set its position at (0,0,0)
+        transform.position = new Vector3(0,0,0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>(); 
+
+        if (_spawnManager == null) {
+            Debug.LogError("The Spawn Manager is NULL.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canfire) 
+        {
+           FireLaser();
+        }
+
+    }
+
+    void CalculateMovement() {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput,verticalInput, 0);
+
+        transform.Translate(direction *_speed* Time.deltaTime);
+
+        if (transform.position.y >= 0) {
+            transform.position = new Vector3(transform.position.x,0,0);
+
+        } else if (transform.position.y <= -3.8f) {
+            transform.position = new Vector3(transform.position.x,-3.8f,0);    
+        }
+
+        if (transform.position.x >= 11.3f) {
+            transform.position = new Vector3(-11.3f,transform.position.y,0);
+        } 
+        else if (transform.position.x <= -11.3f) {
+            transform.position = new Vector3(11.3f,transform.position.y,0);
+        }
+        
+    }
+
+    void FireLaser() {
+
+      _canfire = Time.time + _fireRate;
+
+      if (_isTripleShotActive == true) {
+        Instantiate(_tripleShotPrefab,transform.position,Quaternion.identity);
+      } else {
+
+      Instantiate(_laserPrefab, transform.position + new Vector3(0,1.05f,0), Quaternion.identity);
+      }
+    }
+
+    public void Damage() 
+    {
+        if (_isShieldsActive == true) 
+        {
+            _isShieldsActive = false;
+            _shieldVisualizer.SetActive(false);
+            return;
+        }
+        _lives--;
+
+        if(_lives < 1) {
+            _spawnManager.OnPlayerDeath();
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void TripleShotActive() 
+    {
+        _isTripleShotActive = true;
+        StartCoroutine(TripleShotPowerDownRoutine());
+    }
+
+    IEnumerator TripleShotPowerDownRoutine() 
+    {
+        yield return new WaitForSeconds(5.0f);
+        _isTripleShotActive = false;
+    }
+
+    public void SpeedBoostActive() {
+    //    _isSpeedBoostActive = true;
+        _speed *= _speedMultiplier;
+        StartCoroutine(SpeedBoostPowerDownRoutine());
+    }
+
+    IEnumerator SpeedBoostPowerDownRoutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+  //      _isSpeedBoostActive = false;
+        _speed /= _speedMultiplier;
+    }
+
+    public void ShieldsActive() {
+        _isShieldsActive = true;
+        _shieldVisualizer.SetActive(true);
+    }
+}
